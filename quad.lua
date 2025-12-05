f3e79eabbdbd503673b3bbfd99e00d12b0000097
Paulyn <realjakeojeff@gmail.com>
2025-12-05 12:38:29 +0530

    Creating quad number generator

---
+++ quad.lua
@@ -0,0 +1,13 @@
+local quad = {}
+
+function quad:generateNums()
+    spritesheet = love.graphics.newImage("texture.png")
+    sheetW = spritesheet:getWidth()
+    sheetH = spritesheet:getHeight()
+
+
+    quad.nums = {}
+    for i = 0, 3 do
+        quad.nums[i + 1] = love.graphics.newQuad(i * 16, i * 16, 16, sheetW, sheetH)
+    end
+end
